92485c7c1d39b53dd36ed3ccdf308e65b8a9ea89
Micaela Romina Laganá <micaelal@lagash.com>
2019-12-11 12:00:45 -0300

    fixed reverse function

--- workshopGo.go
+++ workshopGo.go
@@ -15,7 +15,9 @@ func main() {
 func reverse(toConvert string) string {
 	converted := []rune(toConvert)
 	for i, j := 0, len(converted)-1; i < j; i, j = i+1, j-1 {
+		aux := converted[j]
 		converted[j] = converted[i]
+		converted[i] = aux
 
 	}
 	return string(converted)
